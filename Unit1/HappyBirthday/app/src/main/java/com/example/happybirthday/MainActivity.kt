14d36a8ca9e8d4c28c7114f458794efa01d398e2
Danil <ogr.rng2016@yandex.ru>
2025-11-08 22:37:57 +0300

    add greeting

--- Unit1/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -19,10 +19,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
                 }
             }
         }
