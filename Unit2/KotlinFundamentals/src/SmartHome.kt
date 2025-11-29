00d2ddd08337066176c29473c2442ef4075ab93f
Danil <ogr.rng2016@yandex.ru>
2025-11-29 11:53:16 +0300

    define constructor and smarttvdevice

--- Unit2/KotlinFundamentals/src/SmartHome.kt
+++ Unit2/KotlinFundamentals/src/SmartHome.kt
@@ -1,4 +1,15 @@
-class SmartDevice {
+open class SmartDevice(val name: String, val category: String) {
+
+    var deviceStatus = "online"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
     fun turnOn() {
         println("Smart device is turned on.")
     }
@@ -8,8 +19,37 @@ class SmartDevice {
     }
 }
 
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+}
+
 fun main() {
     val smartTvDevice = SmartDevice()
+    println("Device name is: ${smartTvDevice.name}")
     smartTvDevice.turnOn()
     smartTvDevice.turnOff()
 }
