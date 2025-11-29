03523f5b9650b43a3acc0b79b99fcbb48c470d79
Danil <ogr.rng2016@yandex.ru>
2025-11-29 11:59:31 +0300

    define smartlightdevice and smarthome and added extending

--- Unit2/KotlinFundamentals/src/SmartHome.kt
+++ Unit2/KotlinFundamentals/src/SmartHome.kt
@@ -61,6 +61,16 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
         brightnessLevel++
         println("Brightness increased to $brightnessLevel.")
     }
+    override fun turnOn() {
+        deviceStatus = "on"
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+    override fun turnOff() {
+        deviceStatus = "off"
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
 }
 
 class SmartHome(
